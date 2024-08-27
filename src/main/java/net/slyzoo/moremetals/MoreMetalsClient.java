9c7d96d313e30ec70510047e5f7ef51409e4e0e8
slyzoo <gavinhimes@gmail.com>
2024-08-27 16:20:39 -0500

    commited

---
+++ src/main/java/net/slyzoo/moremetals/MoreMetalsClient.java
@@ -0,0 +1,10 @@
+package net.slyzoo.moremetals;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoreMetalsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
